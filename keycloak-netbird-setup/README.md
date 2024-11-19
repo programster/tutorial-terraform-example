6b39a15a602b38d776a8753b0e38aa986fd0c22c
Programster <sdpagent@gmail.com>
2024-11-19 16:24:33 +0000

    initial import

---
+++ keycloak-netbird-setup/README.md
@@ -0,0 +1,10 @@
+This example configures a [Keycloak](https://www.keycloak.org/) server for adding a client for 
+[Netbird](https://docs.netbird.io/selfhosted/identity-providers#keycloak).
+
+## Usage
+
+1. Remove the `.example` extension from the *terraform.tfvars.example* and fill in their values 
+   appropriately.
+1. Run `tofu init` to initialize OpenTofu.
+1. Run `tofu apply` to have OpenTofu create the new realm for Netbird to use.
+1. You should see the new realm when you log into Keycloak.
---
+++ keycloak-netbird-setup/variables.tf
@@ -0,0 +1,80 @@
+
+variable "keycloak_url" {
+    type = string
+    description = "The location of the Keycloak host. E.g. http://keycloak.mydomain.com:8080/ or https://keycloak.mydomain.com/"
+}
+
+variable "keycloak_auth_username" {
+    type = string
+    description = "The username for Terraform/Tofu to connect to Keycloak with."
+    sensitive = true
+}
+
+variable "keycloak_auth_password" {
+    type = string
+    description = "The password for Terraform/Tofu to connect to Keycloak with."
+    sensitive = true
+}
+
+variable "realm_name_slug" {
+    type = string
+    description = "The name to give to the realm created for Netbird. This cannot contain spaces, but can use hyphens."
+}
+
+variable "realm_display_name" {
+    type = string
+    description = "The display name to give to the realm created for Netbird. This can contain spaces."
+}
+
+variable "smtp_host" {
+    type = string
+    description = "The host of your SMTP server. E.g. smtp.gmail.com"
+}
+
+variable "smtp_port" {
+    type = number
+    description = "The port to connect to the SMTP server on."
+}
+
+variable "smtp_from_email" {
+    type = string
+    description = "The email address from which emails should come from. E.g. example@example.com"
+}
+
+variable "smtp_from_display_name" {
+    type = string
+    description = "The name the email should appear from. E.g. 'Keycloak Server'"
+}
+
+variable "smtp_reply_to_email" {
+    type = string
+    description = "The email address use use for reply-to. This should probably be noreply@mydomain.com"
+}
+
+variable "smtp_reply_to_display_name" {
+    type = string
+    description = "The name that should appear for the SMTP reply-to address. This should probably be something like Noreply."
+}
+
+variable smtp_use_starttls {
+    type = bool
+    description = "Whether to use STARTTLS for your SMTP connection. This should not be set to true if smtp_use_ssl is set to true."
+}
+
+variable smtp_use_ssl {
+    type = bool
+    description = "Whether to use SSL for your SMTP connection. This should not be set to true if smtp_use_starttls is set to true."
+}
+
+variable "smtp_username" {
+    type = string
+    description = "The username for your SMTP server. This would be your email address if gmail, but not if you are using AWS SES."
+    sensitive = true
+}
+
+variable "smtp_password" {
+    type = string
+    description = "The password that corresponds to your username for your SMTP server."
+    sensitive = true
+}
+
